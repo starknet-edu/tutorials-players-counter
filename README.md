245b954c179015129086c4cfd6c6c0c35c875e66
drspacemn <dontpanicdao@gmail.com>
2022-03-23 17:34:35 -0600

    non event; unique addresses; unique per event

---
+++ README.md
@@ -0,0 +1,14 @@
+## Tutorial Analytics
+
+First run of golang parser
+```
+$ go run main.go
+50 events with no data at index: [255 256 280 281 427 428 429 430 431 432 478 515 520 525 526 565 642 643 648 651 1001 1002 1003 1249 1347 1354 1355 1356 1357 1404 1405 1439 1440 1441 1442 1443 1466 1558 1559 1576 1577 1578 1579 1612 1632 1633 1650 1651 1725 1750]
+Number of unique addresses per tutorial:
+        - 0x05af2ba86ed7df13ee2b7557a7e6db163e04c5238980c6e9fdeb4bcc040a48bb: 15
+        - 0x04f857652d367b49d2a7de4c8486743f84d53544bc258bfa4de649a24d88b942: 7
+        - 0x03991cf84dea67dda6456876c710f8afd021df235100324618df8ba0fcceb66e: 1811
+Number of unique addresses per event:
+        - new_player: 111
+        - new_validation: 1722
+```
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"math/big"
+	"io/ioutil"
+	"encoding/json"
+)
+
+type Event struct {
+	FromAddress     string   `json:"from_address"`
+	Keys            []string `json:"keys"`
+	Data            []string `json:"data"`
+	ID              int      `json:"id"`
+	BlockHash       string   `json:"blockHash"`
+	TransactionHash string   `json:"transactionHash"`
+	Status          string   `json:"status"`
+}
+
+// @event
+// func new_player(account: felt, rank: felt):
+// end
+
+// @event
+// func new_validation(account: felt, workshop: felt, exercise: felt):
+// end
+type EventMeta struct {
+	IsNewPlayerEvent bool
+	UserAddress string
+	Key string
+}
+
+func main() {
+	eventsFile, err := os.Open("all_events.json")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer eventsFile.Close()
+
+	raw, _ := ioutil.ReadAll(eventsFile)
+	
+	var events []Event
+	json.Unmarshal([]byte(raw), &events)
+
+	m := make(map[string]map[string]EventMeta)
+	var noData []int
+	for i, val := range events {
+		if len(val.Data) > 0 {
+			uniqEvent := EventMeta{
+				IsNewPlayerEvent: len(val.Data) == 2,
+				UserAddress: strToBig(val.Data[0]),
+				Key: val.Keys[0],
+			}
+
+			if evMet, found := m[val.FromAddress]; found {
+				if _, innerFound := evMet[uniqEvent.UserAddress]; !innerFound {
+					evMet[uniqEvent.UserAddress] = uniqEvent
+				}
+				m[val.FromAddress] = evMet
+			} else {
+				im := make(map[string]EventMeta)
+				im[uniqEvent.UserAddress] = uniqEvent
+				m[val.FromAddress] = im
+			}
+		} else {
+			noData = append(noData, i)
+		}
+	}
+
+	fmt.Printf("%v events with no data at index: %v\n", len(noData), noData)
+	fmt.Println("Number of unique addresses per tutorial:")
+	for k, v := range m {
+		fmt.Printf("\t- %v: %v\n", k, len(v))
+	}
+
+	var newPlay, complete int
+	fmt.Println("Number of unique addresses per event:")
+	for _, v := range m {
+		for _, iv := range v {
+			if iv.IsNewPlayerEvent {
+				newPlay++
+			} else {
+				complete++
+			}
+		}
+	}
+	fmt.Printf("\t- new_player: %v\n", newPlay)
+	fmt.Printf("\t- new_validation: %v\n", complete)
+}
+
+func strToBig(val string) string {
+	bigVal, _ := new(big.Int).SetString(val, 10)
+	return fmt.Sprintf("0x%x", bigVal)
+}
